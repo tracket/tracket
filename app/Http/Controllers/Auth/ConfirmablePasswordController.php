0dcfee45ed58d6b314e06d906c8908a242a45379
Dave Glassanos <david.glassanos@gmail.com>
2022-05-28 22:33:43 -0700

    Use theme view for auth pages

--- app/Http/Controllers/Auth/ConfirmablePasswordController.php
+++ app/Http/Controllers/Auth/ConfirmablePasswordController.php
@@ -17,7 +17,7 @@ class ConfirmablePasswordController extends Controller
      */
     public function show()
     {
-        return view('auth.confirm-password');
+        return $this->themeService->view('auth.confirm-password');
     }
 
     /**
--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,16 +3,28 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules;
+use Tracket\Core\Services\SettingsService;
+use App\Models\Role;
+use App\Repositories\RoleRepository;
+use App\Repositories\UserRepository;
 
 class RegisteredUserController extends Controller
 {
+    private SettingsService $settingsService;
+
+    public function __construct(SettingsService $settingsService)
+    {
+        parent::__construct();
+        $this->settingsService = $settingsService;
+    }
+
     /**
      * Display the registration view.
      *
@@ -20,7 +32,14 @@ class RegisteredUserController extends Controller
      */
     public function create()
     {
-        return view('auth.register');
+        if (!$this->settingsService->get('allow_developer_accounts')
+            && !$this->settingsService->get('allow_hiring_manager_accounts')) {
+            abort(404);
+        }
+
+        return $this->themeService->view('auth.register', [
+            'settingsService' => $this->settingsService
+        ]);
     }
 
     /**
@@ -33,17 +52,32 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+        $validated = $request->validate([
+            'name'     => ['required', 'string', 'max:255'],
+            'email'    => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'role'     => ['required', 'string', Rule::in([Role::ROLE_DEVELOPER, Role::ROLE_HIRING_MANAGER])],
         ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
+        $userRepository  = app(UserRepository::class);
+        $roleRepository  = app(RoleRepository::class);
+
+        if (!$this->settingsService->get('allow_developer_accounts') && $validated['role'] === Role::ROLE_DEVELOPER) {
+            throw new \Exception('Cannot create a developer account');
+        }
+
+        if (!$this->settingsService->get('allow_hiring_manager_accounts') && $validated['role'] === Role::ROLE_HIRING_MANAGER) {
+            throw new \Exception('Cannot create a hiring manager account');
+        }
+
+        $user = $userRepository->create([
+            'name'     => $request->name,
+            'email'    => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        $role = $roleRepository->getByName($validated['role']);
+        $user->attachRole($role);
 
         event(new Registered($user));
 
